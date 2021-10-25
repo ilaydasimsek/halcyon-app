caee9a1fa18eab527c79e9b4ae772c1356a7e560
ilaydasimsek <ilaydasimsek4@gmail.com>
2021-10-25 17:38:19 +0300

    Fix template errors

--- MVCTemplate/Custom/MVC.xctemplate/___FILEBASENAME___/___FILEBASENAME___ViewController.swift
+++ MVCTemplate/Custom/MVC.xctemplate/___FILEBASENAME___/___FILEBASENAME___ViewController.swift
@@ -11,4 +11,22 @@ class ___VARIABLE_productName___ViewController: ViewController<___VARIABLE_produ
     required init?(coder: NSCoder) {
         fatalError("Application doesn't use storyboard, init(coder:) shouldn't be called")
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.prepareActions()
+        self.prepareUI()
+    }
+
+    private func prepareActions() {
+        // Add any button actions here
+    }
+
+    private func prepareUI() {
+        // Make any UI specific changes here
+    }
+
+    private func fetch() {
+        // Send the fetch request here, ideally this should return a promise using PromiseKit
+    }
 }
