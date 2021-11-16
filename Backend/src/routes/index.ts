4da68d1295993c74c208359cde2dce3ae43592ed
ilaydasimsek <ilaydasimsek4@gmail.com>
2021-11-16 15:57:39 +0300

    Remove backend since app will use firebase directly

--- Backend/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authRoute from "./authRoute";
-
-const router = express.Router();
-
-router.use("/auth", authRoute);
-
-export default router;
--- Backend/src/types/firebase-user.types.ts
+++
@@ -1,4 +0,0 @@
-export type FirebaseUserDTO = {
-  email: string;
-  password: string;
-};
